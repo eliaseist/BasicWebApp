81b5d743f0e48d919c5de456e9f8f549dedfd65e
Elias <ge96luy@mytum.de>
2022-07-13 10:36:25 +0200

    erster Push laut Aufgabenstellung

--- src/main/java/de/tum/in/ase/eist/ServerApplication.java
+++ src/main/java/de/tum/in/ase/eist/ServerApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ServerApplication {
 
+    //test
+
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
     }
